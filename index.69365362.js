e7cff346f24eda316515390916bfd1dabc682ae2
Roman-Vanchytskyi <104678683+Roman-Vanchytskyi@users.noreply.github.com>
2022-10-28 00:14:26 +0000

    Deploying to gh-pages from @ Roman-Vanchytskyi/goit-js-hw-11@54b9a03e308549bd5f2e8072df468db309a3e684 🚀

--- index.69365362.js
+++
@@ -1,2 +0,0 @@
-$(window).scroll((function(){$(window).scrollTop()>100?$("#back2Top").fadeIn():$("#back2Top").fadeOut()})),$(document).ready((function(){$("#back2Top").click((function(o){return o.preventDefault(),$("html, body").animate({scrollTop:0},"slow"),!1}))}));
-//# sourceMappingURL=index.69365362.js.map
